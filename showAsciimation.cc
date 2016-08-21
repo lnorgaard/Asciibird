e84c8b762d684bb18e808446c1495cf899ead15d
lnorgaard <leemnorgaard@gmail.com>
2016-08-20 17:42:13 -0700

    Use different sprites

--- showAsciimation.cc
+++ showAsciimation.cc
@@ -14,14 +14,18 @@
 
 int main() {
   
-  // Create a Sprite object, have it load sprites/arbitrarySize_mystery2.txt,
+  // Create a Sprite object, have it load high.txt, medium.txt, and low.txt,
   // set the location, and make it move.
-  Sprite mysterySprite_("sprites/randomSprite.txt", 10, 10, true);
+  Sprite highSprite_("sprites/high.txt", 10, 10, true);
+  Sprite mediumSprite_("sprites/medium.txt", 10, 16, true);
+  Sprite lowSprite_("sprites/low.txt", 10, 22, true);
   
   // Create an Asciimation, add the sprite to the asciimation,
   // and show the asciimation!
   Asciimation mysteryAsciimation_;
-  mysteryAsciimation_.addSprite(&mysterySprite_);
+  mysteryAsciimation_.addSprite(&highSprite_);
+  mysteryAsciimation_.addSprite(&mediumSprite_);
+  mysteryAsciimation_.addSprite(&lowSprite_);
   mysteryAsciimation_.show();
   
   return 0;
